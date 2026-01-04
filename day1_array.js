ef940f3d0591c436aec999f9f9c86855fa7f7a40
Prabhat Singh <prabhats1410@gmail.com>
2026-01-04 23:47:42 +0530

    day_array.js complete

---
+++ day1_array.js
@@ -0,0 +1,47 @@
+//console.log('Array Day 1');
+const readline = require("readline");
+
+const val = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// console.log(val);
+
+// for (let i = 0; i < val.length; i++) {
+//     console.log(val[i]);
+// }
+
+// for(x in val){
+//     console.log(x);
+// }
+
+// for(x of val) {
+//     console.log(x);
+// }
+
+// val.forEach(e => {
+//     console.log(e);
+// });
+
+// val.unshift(0); //for insert one extra value in array at start
+
+// val.push(10); //for insert one extra value in array at end
+
+// val.shift(); //for remove first value from array
+
+// val.pop(); //for remove last value from array
+
+// const sliceVal = val.slice(4, val.length);
+// console.log(sliceVal);
+// console.log(val);
+
+// const spliceVal = val.splice(3, 2)
+// console.log(spliceVal);
+// console.log(val);
+
+// const spredVal = [10, 11, 12, ...val] //The spread operator (...) can be used to join arrays
+// console.log(spredVal);
+// console.log(Math.min(...spredVal));
+// console.log(Math.max(...spredVal));
+
+// const indexOfValue = val.indexOf(3);
+// console.log(indexOfValue);
+
